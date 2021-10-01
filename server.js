18d280176aaa90a7ab1ac1cce724490edf646699
thsc47 <thsc47@gmail.com>
2021-10-01 12:10:49 -0300

    mongoose connected to atlas

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const dbConnect = require('./config/db.config')
 require('dotenv').config()
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dbConnect.execute();
 
 app.listen(process.env.PORT, ()=>console.log(`server up: ${process.env.PORT}`))
