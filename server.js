2879f9ff1f4811cc938d1464ae76525e08b8b307
thsc47 <thsc47@gmail.com>
2021-10-01 11:59:00 -0300

    install mongoose

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.listen(process.env.PORT, ()=>console.log(`server up: ${process.env.PORT}`))
