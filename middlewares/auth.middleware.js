c0bc77abf8f55de035d43fa81407e9f9a551bc58
thsc47 <thsc47@gmail.com>
2021-10-01 12:39:59 -0300

    Creating auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,8 +1,17 @@
 const jwt = require("jsonwebtoken");
 const bcript = require("bcryptjs");
 
-class Auth{
-    execute(req, res, next){
-        console.log("TODO")
+class Auth {
+  execute(req, res, next) {
+    const auth = req.get("Authorization");
+    if(!auth){
+        throw new Error("Request without token")
     }
+    const [ ,token] = auth.split(" ")
+    try {
+        
+    } catch (error) {
+        
+    }
+  }
 }
