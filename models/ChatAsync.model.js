f93bf61445b22124a63bbf144a6281e17da2b21a
thsc47 <thsc47@gmail.com>
2021-10-04 18:17:01 -0300

    Async chat model created

---
+++ models/ChatAsync.model.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const chatAsyncSchema = new Schema(
+  {
+    userID1: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    userID2: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    sender: {type: Schema.Types.ObjectId, required: true},
+    message: {type: String, required: true, min: 30, max: 2000},
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("AsyncChat", chatAsyncSchema)
