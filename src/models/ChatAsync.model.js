d6f4843ec8102d13b8f1523d61dfcef68f22b23e
thsc47 <thsc47@gmail.com>
2021-10-12 20:31:09 -0300

    refactor:  rename the exportation in user model

--- src/models/ChatAsync.model.js
+++ src/models/ChatAsync.model.js
@@ -2,8 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const chatAsyncSchema = new Schema(
   {
-    sender: {type: Schema.Types.ObjectId, ref: "User", required: true},
-    reciver: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    sender: {type: Schema.Types.ObjectId, ref: "user", required: true},
+    reciver: {type: Schema.Types.ObjectId, ref: "user", required: true},
     AdvertisementID: {type: Schema.Types.ObjectId, ref: "salesAD", required: true},
     message: {type: String, required: true, min: 30, max: 2000},
   },
