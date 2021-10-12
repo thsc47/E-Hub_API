691be77b06b116ea6225ef4e16876fb70ad81d5a
thsc47 <thsc47@gmail.com>
2021-10-12 20:29:39 -0300

    refactor: rename the fields in the chatAsync model

--- src/models/ChatAsync.model.js
+++ src/models/ChatAsync.model.js
@@ -2,9 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const chatAsyncSchema = new Schema(
   {
-    userID1: {type: Schema.Types.ObjectId, ref: "User", required: true},
-    userID2: {type: Schema.Types.ObjectId, ref: "User", required: true},
-    sender: {type: Schema.Types.ObjectId, required: true},
+    sender: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    reciver: {type: Schema.Types.ObjectId, ref: "User", required: true},
     AdvertisementID: {type: Schema.Types.ObjectId, ref: "salesAD", required: true},
     message: {type: String, required: true, min: 30, max: 2000},
   },
