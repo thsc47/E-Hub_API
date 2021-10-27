38bd8b11aca73ea06b131319101d7172f4402f89
thsc47 <thsc47@gmail.com>
2021-10-26 22:38:20 -0300

    refactor: changing the populate policy in the GetAllAdsFromAllUsers controller

--- src/controllers/ads/getAllAdsfromAllUsers.js
+++ src/controllers/ads/getAllAdsfromAllUsers.js
@@ -2,7 +2,7 @@ const SalesAd = require('../../models/SalesAd.model')
 class GetAllAdsFromAllUsers {
   static async execute() {
     try {
-      const result = await SalesAd.find().populate('userID')
+      const result = await SalesAd.find()
       return result
     } catch (error) {
       throw new Error(error)
