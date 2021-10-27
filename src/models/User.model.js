8d6e4086e0a7ca1f6cce906e7a5e2375ea07b40f
thsc47 <thsc47@gmail.com>
2021-10-26 21:33:51 -0300

    refactor: email is unique

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -3,8 +3,8 @@ const { Schema, model } = require('mongoose')
 const userSchema = new Schema(
   {
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true, unique: true },
-    email: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     age: { type: Number },
     cpf: { type: Number, required: true },
