9f268f068d06a8c76a3fcd5d53babdf1b40fb1f5
thsc47 <thsc47@gmail.com>
2021-10-28 20:54:28 -0300

    fix: ensure the title on createNewAd and search is lower case

--- src/controllers/ads/createNewAd.controller.js
+++ src/controllers/ads/createNewAd.controller.js
@@ -8,8 +8,8 @@ class CreateNewAd {
       const { body } = req
       req.files.map(file => paths.push(file.path))
       const { id } = req.user
-      const salesAd = Object.assign({}, body, { files: paths })
-
+      const lowerTitle = body.title.toLowerCase()
+      const salesAd = Object.assign({}, body, { files: paths }, {title: lowerTitle})
       const payload = { ...salesAd, userID: id }
       const newad = await SalesAd.create(payload)
       return newad
--- src/controllers/search/HomePageSearch.controller.js
+++ src/controllers/search/HomePageSearch.controller.js
@@ -2,7 +2,7 @@ const SalesAd = require('../../models/SalesAd.model')
 
 class HomePageSearch {
   static async execute(req) {
-    const title = new RegExp(`${req.query.search}`)
+    const title = new RegExp(`${req.query.search.toLowerCase()}`)
     return await SalesAd.find({ title })
   }
 }
