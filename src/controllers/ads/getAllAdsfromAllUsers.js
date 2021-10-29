a76912e6125611e8e7bc9fe932e5f88dff4f4141
thsc47 <thsc47@gmail.com>
2021-10-29 10:26:50 -0300

    feat: ensure the GetAllAdsFromAllUsers return a random collection of records

--- src/controllers/ads/getAllAdsfromAllUsers.js
+++ src/controllers/ads/getAllAdsfromAllUsers.js
@@ -2,7 +2,7 @@ const SalesAd = require('../../models/SalesAd.model')
 class GetAllAdsFromAllUsers {
   static async execute() {
     try {
-      const result = await SalesAd.find()
+      const result = await SalesAd.aggregate([{ $sample: { size: 10 } }])
       return result
     } catch (error) {
       throw new Error(error)
